4cee7127fd390ad5353f735ebbee997cd36896ba
JohnpaulIdio <148184514+JohnpaulIdio@users.noreply.github.com>
2024-09-16 19:06:21 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+  // function App() {
+  const App = () => {
+  return  <section className="sm:max-xl:bg-orange-300 md:grid-cols-3 md:items-center md:text-left lg:max-xl:p-9 grid gap-9   ">
+  <div>
+  <img src="https://orig00.deviantart.net/74e3/f/2016/137/1/e/naruto_uzumaki___sage_mode_by_loraxdude-da2s1h6.png"/>
+  </div>
+  <div>
+  <h1 className="mb-4 text-5xl
+  font-medium">Naruto
+  </h1>
+  <p>Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.Naruto was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from September 1999 to November 2014, with its chapters collected in 72 tankōbon volumes. Viz Media licensed the manga for North American production and serialized Naruto in their digital Weekly Shonen Jump magazine. 
+  </p>
+  </div>
+  </section>
+}
+  export default App
